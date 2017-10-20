dd2b7adcd78a8b64b9e74358fafc7ffbf25773df
TenzenIga <arhan-93@list.ru>
2017-10-20 22:39:33 +0300

    first commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -35,7 +35,7 @@ export class UserComponent implements OnInit {
   }
     clear(){ //clear localStorage
       localStorage.clear();
-      document.location.href="/";
+      document.location.href="/vk-test/";
     }
     logIn(){ //redirect user to authorization page
       let redirect_uri =  'https://tenzeniga.github.io/vk-test/';
